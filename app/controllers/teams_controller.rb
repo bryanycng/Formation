d8a324638b2f8e2409a14fbd641e21b2596dd7c4
Bryan Ng <bryan.yc.ng@shaw.ca>
2016-09-29 21:59:08 -0700

    Added User Authentication still a bit of bugs, learning basics at the moment will add more soon

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,9 @@
 class TeamsController < ApplicationController
+
 	def index
-    @teams = Team.all
+    	@teams = Team.all
   	end
+
 	def show
     	@team = Team.find(params[:id])
   	end
@@ -33,6 +35,13 @@ class TeamsController < ApplicationController
     	render 'edit'
   		end
 	end
+
+	def destroy
+   		@team = Team.find(params[:id])
+   		@team.destroy
+ 
+    	redirect_to teams_path
+  	end
 
 	private 
 		def team_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907181850) do
+ActiveRecord::Schema.define(version: 20160930015342) do
 
   create_table "teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.text     "text",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "encrypted_password"
+    t.string   "salt"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
