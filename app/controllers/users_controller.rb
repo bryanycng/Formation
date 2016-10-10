50adbd4bad957ecc363109ee1f37350bec1b8c0f
Bryan Ng <bryan.yc.ng@shaw.ca>
2016-10-10 14:59:59 -0700

    Just added view for listing users/showing profile easy way for me to check database atm gotta check how to check mysql login will find that out soon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
 
+  def index
+      @users = User.all
+  end
+
+  def show
+      @user = User.find(params[:id])
+  end
+
 	def new
-		@user = User.new
+		  @user = User.new
 	end
 
 	def create
@@ -16,6 +24,7 @@ class UsersController < ApplicationController
     	render "new"
   	end
 
+  private
     def user_params
       params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   resources :teams
   resources :users
   get    '/login',   to: 'sessions#new'
