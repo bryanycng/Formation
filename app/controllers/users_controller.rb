6e420083470ab99bc77c1fa306618aa8fe2d5975
Bryan Ng <bryan.yc.ng@shaw.ca>
2016-10-11 03:12:37 -0700

    changed a lot of user modle/migrations to be more coherent with 1 tutorial and having issues with logging in will fix it soon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,32 @@
 class UsersController < ApplicationController
 
   def index
-      @users = User.all
+    @users = User.all
   end
 
   def show
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
-	def new
-		  @user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		  @user = User.new(user_params)
-    	if @user.save
-      		flash[:notice] = "You signed up successfully"
-     	  	flash[:color]= "valid"
-   		else
-     	  	flash[:notice] = "Form is invalid"
-      		flash[:color]= "invalid"
-    	end
-    	render "new"
-  	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
   private
+
     def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006062204) do
+ActiveRecord::Schema.define(version: 20161011092707) do
 
   create_table "teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -20,12 +20,10 @@ ActiveRecord::Schema.define(version: 20161006062204) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "email"
-    t.string   "encrypted_password"
-    t.string   "salt"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
